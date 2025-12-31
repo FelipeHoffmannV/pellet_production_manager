0ccf8c0159fb46ef58b561cfdf9e4cfc8d784468
Felipe <hoffmannfelipe@gmail.com>
2025-12-31 11:31:04 -0300

    Docker implementado a fins de estudo. Próximo passo estruturação do projeto e melhor análise de requisitos

--- app/pellet/models.py
+++ app/pellet/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Usuarios(models.Model):
     id = models.IntegerField(primary_key=True)
     nome = models.CharField(max_length=100)
-    senha = models.IntegerField(max_length=8)
+    senha = models.CharField(max_length=8)
 
     def __str__(self):
         return Usuarios.nome
