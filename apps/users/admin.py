71b0ad37b22502b8d145d8d2fe600037cfe326ce
Felipe <hoffmannfelipe@gmail.com>
2026-03-23 13:18:02 -0300

    Users ajustado

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import userPellet
 
-# Usamos o UserAdmin pronto do Django para herdar todas 
-# as funcionalidades de segurança e layout do Admin original
-@admin.register(User)
+@admin.register(userPellet)
 class MyUserAdmin(UserAdmin):
-    # Aqui você define quais campos aparecem na lista do Admin
     list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
 # Register your models here.
